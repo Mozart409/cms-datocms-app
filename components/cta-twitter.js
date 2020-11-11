5a62fffd883c42efc17ecd4c00c17a83c9d0fa84
Amadeus <38767929+Mozart409@users.noreply.github.com>
2020-11-11 12:19:30 +0100

    updated styles and removed remark for marked

--- components/cta-twitter.js
+++ components/cta-twitter.js
@@ -1,8 +1,8 @@
 export default function ctaTwitter() {
   return (
-    <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-24 lg:px-8 flex items-center justify-between">
+    <div className="max-w-screen-xl mx-auto py-12 lg:py-24 flex items-center justify-between">
       <h2 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 md:text-4xl md:leading-10">
-        <span className="block">Like my content?</span>
+        <span>Like my content?</span>
         <a href="https://twitter.com/marc_van_sax">
           <span className="block text-indigo-600">Follow me on Twitter.</span>
         </a>
--- components/footer.js
+++ components/footer.js
@@ -1,17 +1,17 @@
-import Container from './container'
+import Container from "./container";
 
-export default function Footer () {
+export default function Footer() {
   return (
-    <footer className='bg-accent-1 border-t border-accent-2'>
+    <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
-        <div className='py-28 flex flex-col lg:flex-row items-center'>
-          <h3 className='text-2xl lg:text-4xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2'>
-            Statically Generated with Next.js.
+        <div className="py-28 flex flex-col lg:flex-row items-center">
+          <h3 className="text-xl lg:text-2xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+            Incremental Static Regeneratied with Next.js.
           </h3>
-          <div className='flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2'>
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href='https://github.com/Mozart409/cms-datocms-app'
-              className='mx-3 font-bold hover:underline'
+              href="https://github.com/Mozart409/cms-datocms-app"
+              className="mx-3 hover:underline"
             >
               View on GitHub
             </a>
@@ -19,5 +19,5 @@ export default function Footer () {
         </div>
       </Container>
     </footer>
-  )
+  );
 }
