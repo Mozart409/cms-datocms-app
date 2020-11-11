491819c8279dc69e77f7ddfe37a038af680d456c
Amadeus <38767929+Mozart409@users.noreply.github.com>
2020-11-11 16:11:26 +0100

    centered cta twitter

--- components/cta-twitter.js
+++ components/cta-twitter.js
@@ -1,6 +1,6 @@
 export default function ctaTwitter() {
   return (
-    <div className="max-w-screen-xl mx-auto py-12 lg:py-24 flex items-center justify-between">
+    <div className="max-w-2xl mx-auto py-12 lg:py-24 flex items-center justify-between">
       <h2 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 md:text-4xl md:leading-10">
         <span>Like my content?</span>
         <a href="https://twitter.com/marc_van_sax">
