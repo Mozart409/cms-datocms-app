ef03f59b0fe0d8f1d6246a89ffc88a8beee3df29
Amadeus <maderamadeus@gmail.com>
2020-12-06 20:57:34 +0100

    added splitbee.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-  render () {
+  render() {
     return (
-      <Html lang='en'>
-        <Head />
+      <Html lang="en">
+        <Head>
+          <script async data-api="/_hive" src="/bee.js"></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
